9b4a7b1f8e8846d72b331b0919abe0f5d4f31bde
linapalomo <linapalomog@hotmail.com>
2023-04-15 14:54:52 +0200

    working-all

---
+++ test_df.py
@@ -0,0 +1,13 @@
+import json
+import pandas as pd
+##print initial distribution
+
+with open("data.json", "r") as f:
+    data = json.load(f)
+
+df = pd.DataFrame(data)
+
+# Set pandas to show all rows and columns
+pd.set_option("display.max_rows", None)
+pd.set_option("display.max_columns", None)
+print(df)
